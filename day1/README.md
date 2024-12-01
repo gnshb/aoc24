2ca75352542916283b3127f962eb5700886476a5
gnshb <ch22b002@smail.iitm.ac.in>
2024-12-01 17:08:51 +0530

    Update on 12/01/24 at 17:08:51 by  ganesh

---
+++ day1/README.md
@@ -0,0 +1,7 @@
+# Question 1
+
+real	0m0.019s
+user	0m0.013s
+sys	0m0.011s
+
+# Question 2
---
+++ day1/sol.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# part 1
+
+cut -d" " -f1 input.csv | sort > col1.csv;
+cut -d" " -f4 input.csv | sort > col2.csv;
+
+paste -d"," col* | awk -F',' '{print ($2 - $1)}' | sed 's/-//g' | paste -sd+ - | bc > ans1.txt;
+
+# part 2
+
+echo $(for i in $(uniq col1.csv); do
+	echo "$(grep -c $i col2.csv)*$i*$(grep -c $i col1.csv)"
+done) | sed 's/ /+/g' | bc > ans2.txt;
+
+rm ./col*;
