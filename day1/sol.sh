f2fd87c1c46d7f6f37b30b273d9575464a9777cd
gnshb <ch22b002@smail.iitm.ac.in>
2024-12-01 17:12:57 +0530

    Update on 12/01/24 at 17:12:57 by  ganesh

--- day1/sol.sh
+++ day1/sol.sh
@@ -5,12 +5,12 @@
 cut -d" " -f1 input.csv | sort > col1.csv;
 cut -d" " -f4 input.csv | sort > col2.csv;
 
-paste -d"," col* | awk -F',' '{print ($2 - $1)}' | sed 's/-//g' | paste -sd+ - | bc > ans1.txt;
+paste -d"," col* | awk -F',' '{print ($2 - $1)}' | sed 's/-//g' | paste -sd+ - | bc > ans.txt;
 
 # part 2
 
 echo $(for i in $(uniq col1.csv); do
 	echo "$(grep -c $i col2.csv)*$i*$(grep -c $i col1.csv)"
-done) | sed 's/ /+/g' | bc > ans2.txt;
+done) | sed 's/ /+/g' | bc >> ans.txt;
 
 rm ./col*;
